f821ce05f33d49e4c0ef6ce8e6c9debee4f00676
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2020-05-31 15:38:22 +0200

    feat: change win / loose handler

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -7,13 +7,16 @@ import Word from './word'
 interface GameState {
   word: string,
   history: string[],
-  errors: number
+  errors: number,
+  progress: string
 }
 
+const FullGameState = { start: 'STARTING', end: 'END' }
 const DEFAULT_GAME: GameState = {
   word: '',
   history: [],
-  errors: 0
+  errors: 0,
+  progress: FullGameState.start
 }
 
 function isActualWinned (actual: string, history: string[]): boolean {
@@ -26,47 +29,57 @@ function isActualWinned (actual: string, history: string[]): boolean {
   return isWin
 }
 
-function isLoose (errors: number): boolean {
-  return errors >= 6
-}
-
-const ActionType = { success: 'onGood', reject: 'onFailed', fetch: 'fetchWord' }
-function reducer (state: GameState, action: { type: string, value: string }): GameState {
+const ActionType = { success: 'onGood', reject: 'onFailed', fetch: 'fetchWord', change: 'onChange' }
+function reducer (state: GameState, action: { type: string, value: any }): GameState {
   const slice = state.history.slice()
   slice.push(action.value)
   
   switch (action.type) {
     case ActionType.reject:
+      if (state.errors + 1 >= 6) {
+        return { ...state, progress: FullGameState.end, errors: state.errors + 1 }
+      }
       return { ...state, errors: state.errors + 1, history: slice }
     case ActionType.success:
+      if (isActualWinned(state.word, slice)) {
+        return { ...state, history: slice, progress: FullGameState.end }
+      }
       return { ...state, history: slice }
     case ActionType.fetch: 
       return { ...DEFAULT_GAME, word: action.value }
+    case ActionType.change: 
+      return { ...state, progress: action.value }
     default: throw new Error()
   }
 }
 
 /*
-  call api avec useEffect (api pokemon) + avoir un state ? GAME_INIT, GAME_PROGESS, GAMe_WIN ? 
+  avoir un state ? GAME_INIT, GAME_PROGESS, GAMe_WIN ? 
 */
 export default function Game (): JSX.Element {
   const [state, dispatch] = useReducer(reducer, DEFAULT_GAME)
 
   useEffect(() => {
-    const id: number = Math.floor(Math.random() * (151 - 1 + 1) + 1)
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(({ data }) => {
-      dispatch({ type: ActionType.fetch, value: data.name })
-    }).catch(console.error)
-  }, [/* AFTER CHANGE WHEN GAME_FULL_STATE IS UPDATED (LOOSE FOR EXAMPLE) */])
+    if (state.progress === FullGameState.start) {
+      const id: number = Math.floor(Math.random() * (151 - 1 + 1) + 1)
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(({ data }) => {
+        dispatch({ type: ActionType.fetch, value: data.name })
+      }).catch(console.error)
+    }
+  }, [state.progress])
 
   const input = useRef<HTMLInputElement>(null)
 
   function handleSubmit(event: React.MouseEvent<HTMLInputElement, MouseEvent>): void {
     event.preventDefault()
-    if (input.current === null) return 
+    if (input.current === null) return
     const value = input.current.value
     dispatch({ type: (!state.word.split('').includes(value) ? ActionType.reject : ActionType.success), value })
     input.current.value = ''
+  }
+
+  function handleClick (): void {
+    dispatch({ type: ActionType.change, value: FullGameState.start })
   }
 
   return (
@@ -85,8 +98,18 @@ export default function Game (): JSX.Element {
         />
       </form>
 
+      {state.progress === FullGameState.end && (
+        <div>
+          <p>Vous avez: {(state.errors >= 6) ? 'Perdu ...' : 'Gagné !'}</p>
+          <button 
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+            onClick={handleClick}
+          >Recommencer</button>
+        </div>
+      )}
+
       <p>History: {state.history.join(' ')}</p>
-      <Pendu errors={state.errors} />
+      <Pendu errors={state.errors}/>
     </div>
   )
 }
